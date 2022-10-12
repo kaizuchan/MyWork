0dbe8431765dfd2b7ae2a548e5cdcbcc9c7e327a
mumuyume <kittoarumuutairiku@gmail.com>
2022-10-12 13:47:17 +0900

    ログイン機能

---
+++ src/Controller/UsersController.php
@@ -0,0 +1,16 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Controller;
+
+/**
+ * Users Controller
+ *
+ * @method \App\Model\Entity\User[]|\Cake\Datasource\ResultSetInterface paginate($object = null, array $settings = [])
+ */
+class UsersController extends AppController
+{
+    public function login()
+    {
+    }
+}
