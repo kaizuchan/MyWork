17e34713f3fefbd5eaea47e201290da8b0e50b87
mumuyume <kittoarumuutairiku@gmail.com>
2022-10-19 10:15:37 +0900

    社員登録画面 登録失敗時にデータを引き継ぐ

--- templates/Admin/adduser.php
+++ templates/Admin/adduser.php
@@ -1,12 +1,3 @@
-<?php
-  function getValue($name){
-    $res = "";
-    if(isset($_POST['employee_id'])){
-      $res = $_POST[$name];
-    }
-    echo $res;
-  }
-?>
 <!DOCTYPE html>
 <html lang="ja">
 <head>
@@ -83,47 +74,32 @@
 
     <div class="form-check-group">
       <div class="form-check">
-        <input name="gender" value="0" type="radio" class="form-check-input" id="validationFormCheck2" name="radio-stacked" required>
+        <input name="gender" value="0" type="radio" class="form-check-input" id="validationFormCheck2" name="radio-stacked" required<?= setCheckdGender(0, getValue('gender')); ?>>
         <label for="validationFormCheck2" class="form-check-label">男性</label>
       </div>
       <div class="form-check woman-radioButton">
-        <input name="gender" value="1" type="radio" class="form-check-input" id="validationFormCheck3" name="radio-stacked">
+        <input name="gender" value="1" type="radio" class="form-check-input" id="validationFormCheck3" name="radio-stacked"<?= setCheckdGender(1, getValue('gender')); ?>>
         <label for="validationFormCheck3" class="form-check-label">女性</label>
       </div>
     </div>
-
+<?php
+?>
     
     <h2>生年月日</h2>
     <div class="input-group birth-select mb-3">
       <select name="birthday-year" class="form-select" id="inputGroupSelect01" aria-label="年" required>
-          <option value=""></option>
-          <?php
-          for ($i = 1920; $i <= 2022; $i++) {
-            echo '<option value="'.$i.'">'.$i.'</option>';
-          }
-          ?>
+        <?php echo setNumberOptions(1920, date('Y'), getValue('birthday-year')); ?>
         </select>
       <label class="input-group-text" for="inputGroupSelect01">年</label>
       <select name="birthday-month"  class="form-select" aria-label="月" required>
-          <option value=""></option>
-          <?php
-          for ($i = 1; $i <= 12; $i++) {
-            echo '<option value="'.$i.'">'.$i.'</option>';
-          }
-          ?>
+          <?php echo setNumberOptions(1, 12, getValue('birthday-month')); ?>
         </select>
       <label class="input-group-text" for="inputGroupSelect01">月</label>
         <select name="birthday-date" class="form-select" aria-label="日" required>
-          <option value=""></option>
-          <?php
-          for ($i = 1; $i <= 31; $i++) {
-            echo '<option value="'.$i.'">'.$i.'</option>';
-          }
-          ?>
+          <?php echo setNumberOptions(1, 31, getValue('birthday-date')); ?>
         </select>
       <label class="input-group-text" for="inputGroupSelect01">日</label>
     </div>
-
     
     <h2>住所</h2>
     <div class="input-group mb-3 ">
@@ -132,59 +108,11 @@
     </div>
 
     <div class="input-group mb-3">
-      <label class="input-group-text" for="inputGroupSelect01">都道府県</label>
+      <label class="input-group-text" for="inputGroupSelect01"(>都道府県</label>
       <select name="prefecture_id" class="form-select p-region" id="inputGroupSelect01" aria-label="都道府県" required>
-        <option value=""></option>
-        <option value="1">北海道</option>
-        <option value="2">青森県</option>
-        <option value="3">岩手県</option>
-        <option value="4">宮城県</option>
-        <option value="5">秋田県</option>
-        <option value="6">山形県</option>
-        <option value="7">福島県</option>
-        <option value="8">茨城県</option>
-        <option value="9">栃木県</option>
-        <option value="10">群馬県</option>
-        <option value="11">埼玉県</option>
-        <option value="12">千葉県</option>
-        <option value="13">東京都</option>
-        <option value="14">神奈川県</option>
-        <option value="15">新潟県</option>
-        <option value="16">富山県</option>
-        <option value="17">石川県</option>
-        <option value="18">福井県</option>
-        <option value="19">山梨県</option>
-        <option value="20">長野県</option>
-        <option value="21">岐阜県</option>
-        <option value="22">静岡県</option>
-        <option value="23">愛知県</option>
-        <option value="24">三重県</option>
-        <option value="25">滋賀県</option>
-        <option value="26">京都府</option>
-        <option value="27">大阪府</option>
-        <option value="28">兵庫県</option>
-        <option value="29">奈良県</option>
-        <option value="30">和歌山県</option>
-        <option value="31">鳥取県</option>
-        <option value="32">島根県</option>
-        <option value="33">岡山県</option>
-        <option value="34">広島県</option>
-        <option value="35">山口県</option>
-        <option value="36">徳島県</option>
-        <option value="37">香川県</option>
-        <option value="38">愛媛県</option>
-        <option value="39">高知県</option>
-        <option value="40">福岡県</option>
-        <option value="41">佐賀県</option>
-        <option value="42">長崎県</option>
-        <option value="43">熊本県</option>
-        <option value="44">大分県</option>
-        <option value="45">宮崎県</option>
-        <option value="46">鹿児島県</option>
-        <option value="47">沖縄県</option>
+        <?php echo setPrefectureOptions(getValue('prefecture_id')); ?>
       </select>
     </div>
-
     <div class="input-group mb-3 ">
       <span class="input-group-text" id="basic-addon1">市区町村</span>
       <input name="city" value="<?= getValue('city') ?>" type="text" id="validationTextarea" class="form-control p-locality p-street-address" placeholder="市区町村" aria-label="市区町村" aria-describedby="basic-addon1" required>
