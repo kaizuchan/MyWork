f85a2a4fb6e966fd0b3d8c4d4a651ee289c17ae6
opipinopi <toyotaka.me@gmail.com>
2022-10-28 11:55:36 +0900

    change: 社員編集　微調整

--- templates/Admin/adduser.php
+++ templates/Admin/adduser.php
@@ -144,7 +144,7 @@
         
         <div class="input-group mb-3 ">
           <span class="input-group-text" id="basic-addon1">パスワード(確認)</span>
-          <input type="password" id="password_confirm" class="form-control" placeholder="" aria-describedby="basic-addon1" pattern="(?=.*?[a-z])(?=.*?[A-Z])(?=.*?\d)[a-zA-Z\d]{8,}" required oninput="CheckPassword(this)">
+          <input type="password" id="password_confirm" class="form-control" placeholder="" aria-describedby="basic-addon1" pattern="(?=.*?[a-z])(?=.*?[A-Z])(?=.*?\d)[a-zA-Z\d]{8,}" required oninput="CheckPassword(this)" required>
         </div>
         <div class="inputMessage"><p>半角英大文字、半角英小文字、半角数字を必ず含み、8文字以上（その他は文字含ませない）</p></div>
         <div id="passwordCheck">
